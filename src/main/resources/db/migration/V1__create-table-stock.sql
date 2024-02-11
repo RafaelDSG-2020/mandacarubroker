4bf4abc925bff28e60065c5ca4d41680f6455a54
Rafael da Silva <rafagomes53@alu.ufc.br>
2024-02-11 19:50:03 -0300

    Ajustando checkstyle, tabela de banco de dados e questão de segurança

--- src/main/resources/db/migration/V1__create-table-stock.sql
+++ src/main/resources/db/migration/V1__create-table-stock.sql
@@ -1,6 +1,6 @@
 CREATE TABLE stock(
-                      id VARCHAR PRIMARY KEY,
-                      symbol VARCHAR NOT NULL,
-                      company_name VARCHAR NOT NULL,
-                      price FLOAT NOT NULL
+                      id VARCHAR(255) PRIMARY KEY,
+                      symbol VARCHAR(50) NOT NULL,
+                      company_name VARCHAR(255) NOT NULL,
+                      price DOUBLE PRECISION NOT NULL
 );
